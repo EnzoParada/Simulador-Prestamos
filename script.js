5f75daf5013d45e298fcb9fd5de5a071c7305f58
enzo <paradaenzo26@outlook.com>
2025-05-07 16:54:54 -0300

    Actualize el archivo script.js refactorize la validación de inputs a función genérica

--- script.js
+++ script.js
@@ -1,32 +1,17 @@
-const inputTexto = document.getElementById('nombre');
 
-inputTexto.addEventListener('input', function() {
-  const textoIngresado = this.value;
-  const soloTexto = /^[a-zA-Z\s]*$/.test(textoIngresado);
+function validarInput( inputId,regex){
+  const inputElement = document.getElementById(inputId);
 
-  if (!soloTexto) {
-    this.value = textoIngresado.slice(0, -1);
-  }
-});
+  inputElement.addEventListener('input', function(){
+    const valorIngresado = this.value;
+    const esValido= regex.test(valorIngresado);
 
-const inputNum = document.getElementById("dni");
-
-inputNum.addEventListener('input', function(){
-    const numIngresado = this.value;
-    const soloNum = /^[0-9]*$/.test(numIngresado);
-
-    if(!soloNum){
-        this.value= numIngresado.slice(0, -1);
+    if(!esValido){
+      this.value= valorIngresado.slice(0,-1);
     }
-});
 
-const inputDni= document.getElementById('numero');
-
-inputDni.addEventListener('input', function(){
-    const dniIngresado= this.value;
-    const soloDni = /^[0-9]*$/.test(dniIngresado);
-
-    if(!soloDni){
-        this.value = dniIngresado.slice(0, -1);
-    }
-});
+  });
+}
+validarInput('nombre', /^[a-zA-Z\s]*$/);
+validarInput('dni', /^[0-9]*$/);
+validarInput('numero', /^[0-9]*$/)
