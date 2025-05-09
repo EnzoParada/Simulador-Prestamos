3a6976710ec14cc0df35eccae67ab9787f581953
enzo <paradaenzo26@outlook.com>
2025-05-09 16:15:03 -0300

    valida rango numerico en input 'monto'

--- script.js
+++ script.js
@@ -3,15 +3,28 @@ function validarInput( inputId,regex){
   const inputElement = document.getElementById(inputId);
 
   inputElement.addEventListener('input', function(){
-    const valorIngresado = this.value;
-    const esValido= regex.test(valorIngresado);
+    let valorIngresado = this.value;
 
-    if(!esValido){
-      this.value= valorIngresado.slice(0,-1);
+    if (inputId === 'monto') {
+      this.value = valorIngresado.replace(/[^0-9]/g, '');
+      valorIngresado = this.value; 
+
+      const numeroIngresado = parseInt(valorIngresado, 10);
+      if (valorIngresado.length > 0 && (isNaN(numeroIngresado) || numeroIngresado < 1000 || numeroIngresado > 1000000)) {
+        
+        inputElement.classList.add('input-invalido'); 
+      } else {
+        inputElement.classList.remove('input-invalido');
+      }
+    } else if (regex) {
+      if (!regex.test(valorIngresado)) {
+        this.value = valorIngresado.slice(0, -1);
+      }
     }
-
   });
 }
+
 validarInput('nombre', /^[a-zA-Z\s]*$/);
 validarInput('dni', /^[0-9]*$/);
-validarInput('numero', /^[0-9]*$/)
+validarInput('numero', /^[0-9]*$/);
+validarInput('monto');
