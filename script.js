f03475ed081c7e464c6d5304ad7fcdcf0f96d214
enzo <paradaenzo26@outlook.com>
2025-05-11 03:34:38 -0300

    creacion de validacion de edad y mejora en manejo de rangos

--- script.js
+++ script.js
@@ -1,18 +1,29 @@
-
-function validarInput( inputId,regex){
+function validarInput(inputId, regex, min, max) {
   const inputElement = document.getElementById(inputId);
 
-  inputElement.addEventListener('input', function(){
+  inputElement.addEventListener('input', function () {
     let valorIngresado = this.value;
 
-    if (inputId === 'monto') {
+    if (inputId === 'monto' || (min !== undefined && max !== undefined)) {
       this.value = valorIngresado.replace(/[^0-9]/g, '');
-      valorIngresado = this.value; 
+      valorIngresado = this.value;
 
       const numeroIngresado = parseInt(valorIngresado, 10);
-      if (valorIngresado.length > 0 && (isNaN(numeroIngresado) || numeroIngresado < 1000 || numeroIngresado > 1000000)) {
-        
-        inputElement.classList.add('input-invalido'); 
+      const tieneValor = valorIngresado.length > 0;
+      const fueraDeRango = isNaN(numeroIngresado) || (min !== undefined && max !== undefined && (numeroIngresado < min || numeroIngresado > max));
+
+      if (tieneValor && min !== undefined && max !== undefined && fueraDeRango) {
+        inputElement.classList.add('input-invalido');
+      } else {
+        inputElement.classList.remove('input-invalido');
+      }
+    } else if (inputId === 'edad') {
+      this.value = valorIngresado.replace(/[^0-9]/g, '');
+      valorIngresado = this.value;
+
+      const edadIngresada = parseInt(valorIngresado, 10);
+      if (valorIngresado.length > 0 && (isNaN(edadIngresada) || edadIngresada < 18 || edadIngresada > 99)) {
+        inputElement.classList.add('input-invalido');
       } else {
         inputElement.classList.remove('input-invalido');
       }
@@ -27,4 +38,5 @@ function validarInput( inputId,regex){
 validarInput('nombre', /^[a-zA-Z\s]*$/);
 validarInput('dni', /^[0-9]*$/);
 validarInput('numero', /^[0-9]*$/);
-validarInput('monto');
+validarInput('monto', undefined, 1000, 1000000);
+validarInput('edad');
